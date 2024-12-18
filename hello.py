dfb60d80f269148164613ad43cf0e1b835e542c9
github-cloudlabsuser-1273 <168013408+github-cloudlabsuser-1273@users.noreply.github.com>
2024-12-18 06:03:03 +0000

    HELLO WORD

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 # print hellow word
 print("Hello, World!")
 # Run the script
 # python hello.py
 # Output: Hello, World!
+=======
+print("Hello, World!")
+>>>>>>> 755bf7cca317210f938e05adccf4ce9575816244
